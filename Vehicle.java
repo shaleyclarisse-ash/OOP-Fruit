469fe184568ffc022e22133931494a89060c9fd2
Ashley <shaleyclarisse@gmail.com>
2025-11-04 19:26:43 +0800

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,13 @@
+class Vehicle {
+  public static void main (String [] args) {
+    String Brand;
+    String type;
+    int year;
+  }
+
+  void displayVehicle(){
+    System.out.println ("Vehicle Brand: " + Brand);
+    System.out.println ("Vehicle type: " + type);
+    System.out.println ("Year: " + year);
+  }
+}
