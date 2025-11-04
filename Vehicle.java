57e789fc6a857c9231aa8ff89ed2d8e51833474d
Ashley <shaleyclarisse@gmail.com>
2025-11-04 19:37:50 +0800

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,13 +1,12 @@
 class Vehicle {
-  public static void main (String [] args) {
     String Brand;
     String type;
     int year;
-  }
-
+  
   void displayVehicle(){
     System.out.println ("Vehicle Brand: " + Brand);
     System.out.println ("Vehicle type: " + type);
     System.out.println ("Year: " + year);
+    System.out.println("-----------------------");
   }
 }
