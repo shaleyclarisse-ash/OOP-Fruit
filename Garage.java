1587b77782d169dba4c2489b5ab020e578a06579
Ashley <shaleyclarisse@gmail.com>
2025-11-04 19:27:02 +0800

    Create Garage.java

---
+++ Garage.java
@@ -0,0 +1,12 @@
+class Garage {
+  public static void main (String [] args){
+    String GarageName;
+  }
+
+void displayGarage(){
+  System.out.println("Garage Name: " + GarageName);
+}
+void parkVehicle(){
+  System.out.println("Parking vehicle in the garage");
+}
+}
