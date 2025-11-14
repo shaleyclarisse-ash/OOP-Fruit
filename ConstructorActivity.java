ffef4160e48a53a2031d950e965f0ebd8f9449d3
Ashley <shaleyclarisse@gmail.com>
2025-11-14 14:32:09 +0800

    Create ConstructorActivity.java

---
+++ ConstructorActivity.java
@@ -0,0 +1,29 @@
+public class Movie {
+  String title;
+  String genre;
+  double rating;
+  
+  public Movie(String favtitle, String favgenre, double favrating){
+    title = favtitle;
+    genre = favgenre;
+    rating = favrating;
+  }
+  
+  public void displayInfo() {
+    System.out.println("```Movie```");
+    System.out.println("Title: " + title);
+    System.out.println("Genre: " + genre);
+    System.out.println("Rating: " + rating);
+    System.out.println("````````````````");
+  }
+}
+public class Main{
+  public static void main(String[] args) {
+    
+    Movie favMovie1 = new Movie("Cruella", "Comedy Crime", 9.5);
+    Movie favMovie2 = new Movie("Ballerina", "Action/Thriller", 9.8);
+    
+    favMovie1.displayInfo();
+    favMovie2.displayInfo();
+  }
+}
