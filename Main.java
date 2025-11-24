5c088f1db21c776fc87830b29df111417f6df976
Ashley <shaleyclarisse@gmail.com>
2025-11-24 19:14:50 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String [] args ) {
+    Student s1 = new Student("Jhon Smith", "65555", "4th Year", "BSIT", "OOP", 96, 98 );
+    
+    s1.introduceStudent();
+    s1.calculateGrade();
+    
+  }
+}
